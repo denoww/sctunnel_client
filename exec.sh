86c3146359235e06db09a8ea3cb49864f386dde9
Rodrigo <rodrigo@seucondominio.com.br>
2023-06-09 00:29:56 -0300

    refatração metodos da lista

--- exec.sh
+++ exec.sh
@@ -53,64 +53,58 @@ montarUrl(){
 tunnelDevices() {
   echo "==========================="
   echo 'tunnelDevices'
-  URL=$(montarUrl 'get_tunnel_devices')
-  devices=$(curl -s $URL 2>&1 | jq -c '.devices?')
+  url=$(montarUrl 'get_tunnel_devices')
+  devices=$(curl -s $url 2>&1 | jq -c '.devices?')
 
   readarray -t list < <(echo $devices | jq -c '.[]')
   for device in "${list[@]}"; do
-    _jq() {
-      echo ${device} | jq -r ${1}
-    }
-
-    deviceId=$(_jq '.id')
-
-    host=$(_jq '.host')
-    if [ -n $host ]; then
-
-      protocol=$(echo $host | cut -d : -f 1)
-      ip=$(echo $host | cut -d : -f 2)
-      ip=${ip#*//} # sem http ou https ou //
-      porta=$(echo $host | cut -d : -f 3)
-      if [ -z $porta ]; then
-        porta=80
-      fi
-    fi
-    tunnelPorta=$(findTunnelPort)
-    newHost="${SC_TUNNEL_ADDRESS}:${tunnelPorta}"
-    hostPortaDevice="${ip}:${porta}"
-
-    newHostToSave=$newHost
-
-    if [ -n $protocol ]; then
-      newHostToSave="${protocol}://${newHostToSave}"
-    fi
-
-    echo
-    echo "¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨"
-    echo "${hostPortaDevice} -> ${newHostToSave}"
-    echo
-    ssh -N -o ServerAliveInterval=20 -i "$SC_TUNNEL_PEM_FILE" -R $tunnelPorta:$hostPortaDevice ubuntu@$SC_TUNNEL_ADDRESS > /dev/null &
-
-
-    updateHostOnErp $deviceId $newHostToSave
+    tunnelDevice $device
   done
-  echo
 }
 
-updateHostOnErp(){
-  deviceId=$1
-  newHost=$2
-  echo "Salvando equipamento $deviceId com -> $newHost"
+tunnelDevice(){
+  device=$1
+  _jq() {
+    echo ${device} | jq -r ${1}
+  }
+
+  deviceId=$(_jq '.id')
+
+  address=$(_jq '.address')
+  if [ -n $address ]; then
+
+    protocol=$(echo $address | cut -d : -f 1)
+    ip=$(echo $address | cut -d : -f 2)
+    ip=${ip#*//} # sem http ou https ou //
+    porta=$(echo $address | cut -d : -f 3)
+    if [ -z $porta ]; then
+      porta=80 # se não tiver porta usa a 80
+    fi
+  fi
+  tunnelPorta=$(findTunnelPort)
+  new_address="${SC_TUNNEL_ADDRESS}:${tunnelPorta}"
+  hostPortaDevice="${ip}:${porta}"
+
+  address_with_protocol=$new_address
+
+  if [ -n $protocol ]; then
+    address_with_protocol="${protocol}://${address_with_protocol}"
+  fi
+
+  echo
+  echo "¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨"
+  echo "${hostPortaDevice} -> ${address_with_protocol}"
+  echo
+  ssh -N -o ServerAliveInterval=20 -i "$SC_TUNNEL_PEM_FILE" -R $tunnelPorta:$hostPortaDevice ubuntu@$SC_TUNNEL_ADDRESS > /dev/null &
+
+
+  echo "Salvando equipamento $deviceId com -> $new_address"
   echo "¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨"
   echo
-
   url=$(montarUrl 'update_tunnel_devices')
 
-  curl -X POST -H "Content-Type: application/json" -d '{"id": "'$deviceId'", "host": "'$newHost'", "cliente_id": "'$CLIENTE_ID'"}' $url
-  echo
-
+  curl -X POST -H "Content-Type: application/json" -d '{"id": "'$deviceId'", "address": "'$address_with_protocol'", "cliente_id": "'$CLIENTE_ID'"}' $url
 }
-
 
 tunnelDevices
 
