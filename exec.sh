fa0733d4594715e11d33a72389e93b3fc5584dcd
Rodrigo <rodrigo@seucondominio.com.br>
2023-06-11 11:29:32 -0300

    funcionando com erp

--- exec.sh
+++ exec.sh
@@ -51,15 +51,20 @@ montarUrl(){
 }
 
 tunnelDevices() {
-  echo "==========================="
-  echo 'tunnelDevices'
+  echo "Procurando equipamentos para fazer tunnel..."
   url=$(montarUrl 'get_tunnel_devices')
   devices=$(curl -s $url 2>&1 | jq -c '.devices?')
 
   readarray -t list < <(echo $devices | jq -c '.[]')
-  for device in "${list[@]}"; do
-    tunnelDevice $device
-  done
+
+  if [ ${#list[@]} -eq 0 ]; then
+    echo "Nenhum tunnel para fazer (lista vazia)"
+  else
+    for device in "${list[@]}"; do
+      tunnelDevice $device
+    done
+  fi
+
 }
 
 tunnelDevice(){
@@ -71,39 +76,42 @@ tunnelDevice(){
   deviceId=$(_jq '.id')
 
   address=$(_jq '.address')
-  if [ -n $address ]; then
+  port=$(_jq '.port')
+  ip=$(_jq '.ip')
+  tunnel_me=$(_jq '.tunnel_me')
+  # if [ -n $address ]; then
 
-    protocol=$(echo $address | cut -d : -f 1)
-    ip=$(echo $address | cut -d : -f 2)
-    ip=${ip#*//} # sem http ou https ou //
-    porta=$(echo $address | cut -d : -f 3)
-    if [ -z $porta ]; then
-      porta=80 # se não tiver porta usa a 80
-    fi
-  fi
+  #   protocol=$(echo $address | cut -d : -f 1)
+  #   ip=$(echo $address | cut -d : -f 2)
+  #   ip=${ip#*//} # sem http ou https ou //
+  #   port=$(echo $address | cut -d : -f 3)
+  #   if [ -z $port ]; then
+  #     port=80 # se não tiver port usa a 80
+  #   fi
+  # fi
   tunnelPorta=$(findTunnelPort)
   new_address="${SC_TUNNEL_ADDRESS}:${tunnelPorta}"
-  hostPortaDevice="${ip}:${porta}"
+  hostPortaDevice="${ip}:${port}"
 
-  address_with_protocol=$new_address
+  # address_with_protocol=$new_address
 
-  if [ -n $protocol ]; then
-    address_with_protocol="${protocol}://${address_with_protocol}"
-  fi
+  # if [ -n $protocol ]; then
+  #   address_with_protocol="${protocol}://${address_with_protocol}"
+  # fi
 
   echo
   echo "¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨"
-  echo "${hostPortaDevice} -> ${address_with_protocol}"
   echo
   ssh -N -o ServerAliveInterval=20 -i "$SC_TUNNEL_PEM_FILE" -R $tunnelPorta:$hostPortaDevice ubuntu@$SC_TUNNEL_ADDRESS > /dev/null &
 
 
-  echo "Salvando equipamento $deviceId com -> $new_address"
+  echo "Definindo novo endereço de #$deviceId no erp"
+  echo "${hostPortaDevice} -> ${new_address}"
   echo "¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨"
   echo
   url=$(montarUrl 'update_tunnel_devices')
 
-  curl -X POST -H "Content-Type: application/json" -d '{"id": "'$deviceId'", "address": "'$address_with_protocol'", "cliente_id": "'$CLIENTE_ID'"}' $url
+  curl -X POST -H "Content-Type: application/json" -d '{"id": "'$deviceId'", "address": "'$new_address'", "cliente_id": "'$CLIENTE_ID'"}' $url
 }
 
 tunnelDevices
