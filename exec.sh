d5a63fa400bd0c26f9be8b1e03e25cdf356bf9d3
Rodrigo <rodrigo@seucondominio.com.br>
2023-06-11 17:01:12 -0300

    Checando conexões ativas

--- exec.sh
+++ exec.sh
@@ -1,9 +1,7 @@
 echo ''
-echo '---------------------------------------------------------------------------------'
-echo '---------------------------------------------------------------------------------'
+echo '=================================================================================='
 echo "Sincronizando equipamentos ($(date))"
-echo '---------------------------------------------------------------------------------'
-echo '---------------------------------------------------------------------------------'
+echo '=================================================================================='
 echo ''
 
 # clear config
@@ -11,8 +9,9 @@ echo ''
 # echo $new_config | jq '.' > config.json
 
 
+DIR=$(dirname "$0")
 function get_config {
-  config=$(jq -r '' "config.json")
+  config=$(jq -r '' "$DIR/config.json")
   echo $config | jq '.'$1''
 }
 
@@ -21,9 +20,12 @@ TOKEN=$(get_config "sc_server.token" | tr -d '"')
 CLIENTE_ID=$(get_config "sc_server.cliente_id" | tr -d '"')
 
 SC_TUNNEL_ADDRESS=$(get_config "sc_tunnel_server.host" | tr -d '"')
+SC_TUNNEL_USER=$(get_config "sc_tunnel_server.user" | tr -d '"')
 # SC_TUNNEL_PEM_FILE="$projectPath/$(get_config "sc_tunnel_server.pem_file" | tr -d '"')"
-SCRIPTPATH=$(dirname "$SCRIPT")
-SC_TUNNEL_PEM_FILE="${SCRIPTPATH}/scTunnel.pem"
+SC_TUNNEL_PEM_FILE="${DIR}/scTunnel.pem"
+CONEXOES_FILE="${DIR}/conexoes.txt"
+
+
 
 
 
@@ -40,20 +42,20 @@ SC_TUNNEL_PEM_FILE="${SCRIPTPATH}/scTunnel.pem"
 
 # done
 
-findTunnelPort() {
+find_tunnel_port() {
   portas=$(curl -s --request GET http://${SC_TUNNEL_ADDRESS}:3020/unused_ports?qtd=1 )
   echo $portas | jq -r '.portas[0]'
 }
 
-montarUrl(){
+montar_erp_url(){
   path=$1
   echo "$HOST/portarias/${path}.json?token=$TOKEN&cliente_id=$CLIENTE_ID"
 }
 
-tunnelDevices() {
+updateDevices() {
   echo "Procurando equipamentos para fazer tunnel..."
-  url=$(montarUrl 'get_tunnel_devices')
-  devices=$(curl -s $url 2>&1 | jq -c '.devices?')
+  get_url=$(montar_erp_url 'get_tunnel_devices')
+  devices=$(curl -s $get_url 2>&1 | jq -c '.devices?')
 
   readarray -t list < <(echo $devices | jq -c '.[]')
 
@@ -61,24 +63,27 @@ tunnelDevices() {
     echo "Nenhum tunnel para fazer (lista vazia)"
   else
     for device in "${list[@]}"; do
-      tunnelDevice $device
+      update_tunnel $device
     done
   fi
 
 }
 
-tunnelDevice(){
+
+update_tunnel(){
   device=$1
-  _jq() {
+  getDevice() {
     echo ${device} | jq -r ${1}
   }
 
-  deviceId=$(_jq '.id')
+  device_id=$(getDevice '.id')
+  # port=$(getDevice '.port')
+  # ip=$(getDevice '.ip')
+  device_host=$(getDevice '.host')
+  tunnel_me=$(getDevice '.tunnel_me')
+  tipo=$(getDevice '.tipo')
+  tunnel_me=$(getDevice '.tunnel_me')
 
-  address=$(_jq '.address')
-  port=$(_jq '.port')
-  ip=$(_jq '.ip')
-  tunnel_me=$(_jq '.tunnel_me')
   # if [ -n $address ]; then
 
   #   protocol=$(echo $address | cut -d : -f 1)
@@ -89,36 +94,141 @@ tunnelDevice(){
   #     port=80 # se não tiver port usa a 80
   #   fi
   # fi
-  tunnelPorta=$(findTunnelPort)
-  new_address="${SC_TUNNEL_ADDRESS}:${tunnelPorta}"
-  hostPortaDevice="${ip}:${port}"
+  # device_host="${ip}:${port}"
 
-  # address_with_protocol=$new_address
+  # address_with_protocol=$tunnel_address
 
   # if [ -n $protocol ]; then
   #   address_with_protocol="${protocol}://${address_with_protocol}"
   # fi
 
+
+
   echo
   echo "¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨"
-  echo
-  ssh -N -o ServerAliveInterval=20 -i "$SC_TUNNEL_PEM_FILE" -R $tunnelPorta:$hostPortaDevice ubuntu@$SC_TUNNEL_ADDRESS > /dev/null &
+  if [ "$tunnel_me" != null ]; then
 
+    if [ "$tunnel_me" = false ]; then
+      disconnect_old_tunnel $device_host
+    fi
 
-  echo "Definindo novo endereço de #$deviceId no erp"
-  echo "${hostPortaDevice} -> ${new_address}"
+    if [ "$tunnel_me" = true ]; then
+      disconnect_old_tunnel $device_host
+      connect_tunnel $device_host
+    fi
+
+    update_no_erp $device_id $tipo $tunnel_address
+
+  else
+    garantir_conexao_do_device $device_host $device_id $tipo $tunnel_address
+  fi
   echo "¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨"
   echo
-  url=$(montarUrl 'update_tunnel_devices')
 
-  curl -X POST -H "Content-Type: application/json" -d '{"id": "'$deviceId'", "address": "'$new_address'", "cliente_id": "'$CLIENTE_ID'"}' $url
 }
 
-tunnelDevices
+update_no_erp(){
+  device_id=$1
+  tipo=$2
+  tunnel_address=$3
+  update_url=$(montar_erp_url 'update_tunnel_devices')
+  curl -X POST -H "Content-Type: application/json" -d '{"id": "'$device_id'", "tipo": "'$tipo'", "tunnel_address": "'$tunnel_address'", "cliente_id": "'$CLIENTE_ID'"}' $update_url &> /dev/null
+}
+
+garantir_conexao_do_device(){
+  # Garantir conexão
+  device_host=$1
+  device_id=$2
+  tipo=$3
+  tunnel_address=$4
+
+  linha=$(grep "device_host:$device_host" $CONEXOES_FILE)
+  # echo $linha
+
+  pid=$(echo "$linha" | sed 's/pid:\([^§]\+\).*/\1/')
+
+  if kill -0 "$pid" >/dev/null 2>&1; then
+    echo "Tunnel ativo de $device_host - PID $pid"
+  else
+    echo "Caiu Tunnel de $device_host - PID $pid"
+
+    disconnect_old_tunnel $device_host
+    connect_tunnel $device_host
+    update_no_erp $device_id $tipo $tunnel_address
+  fi
+
+}
 
 
-echo ''
-echo '---------------------------------------------------------------------------------'
-echo "OK OK OK OK OK OK OK OK OK OK OK OK OK ($(date))"
-echo '---------------------------------------------------------------------------------'
-echo ''
+connect_tunnel(){
+  device_host=$1
+  tunnel_porta=$(find_tunnel_port)
+  tunnel_address="${SC_TUNNEL_ADDRESS}:${tunnel_porta}"
+  ssh -N -o ServerAliveInterval=20 -i "$SC_TUNNEL_PEM_FILE" -R $tunnel_porta:$device_host $SC_TUNNEL_USER@$SC_TUNNEL_ADDRESS > /dev/null &
+  pid=$!
+  salvar_conexao_arquivo $pid $device_host $tunnel_porta
+  echo
+  echo "Definindo novo endereço de #$device_id no erp"
+  echo "${device_host} -> ${tunnel_address}"
+}
+
+remover_conexao_arquivo(){
+  device_host=$1
+  device_host_regex=$(echo "$device_host" | sed 's/\./\\./g')
+  sed -i "/device_host:$device_host_regex/d" "$CONEXOES_FILE"
+  # procurar e remover
+}
+
+salvar_conexao_arquivo(){
+  pid=$1
+  device_host=$2
+  tunnel_porta=$3
+  echo "pid:${pid}§§§§device_host:${device_host}§§§§tunnel_porta:${tunnel_porta}" >> $CONEXOES_FILE
+}
+
+# salvar_conexao(){
+#   pid=$1
+#   device_host=$2
+#   tipo=$3
+#   device_id=$4
+#   tunnel_porta=$5
+#   CONEXOES_FILE="${DIR}/conexoes.txt"
+#   echo "pid:${pid}§§§§device_host:${device_host}§§§§tipo:${tipo}§§§§device_id:${device_id}§§§§tunnel_porta:${tunnel_porta}" >> $CONEXOES_FILE
+# }
+
+disconnect_old_tunnel(){
+  device_host=$1
+  echo "Removendo tunnel antigo de $device_host"
+
+
+  pids=()
+  while IFS= read -r line; do
+    pids+=("$line")
+  done < <(ps aux | grep "$device_host $SC_TUNNEL_USER@$SC_TUNNEL_ADDRESS" | awk '{print $2}')
+
+  # Exibir o array de PIDs
+  for pid in "${pids[@]}"; do
+    # $(kill -9 $pid > /dev/null &)
+
+    if kill -0 $pid 2>/dev/null; then
+      kill $pid
+      echo "processo $pid encerrado"
+    # else
+      # echo "O processo com o PID fornecido não está em execução."
+    fi
+  done
+  remover_conexao_arquivo $device_host
+
+}
+
+updateDevices
+
+
+
+
+
+echo
+echo '=================================================================================='
+echo "FIM ($(date))"
+echo '=================================================================================='
+echo
