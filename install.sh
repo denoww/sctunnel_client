761d7c288cf2a85ade49c7712befce52784e1c2f
Rodrigo <denoww@gmail.com>
2025-05-09 08:10:59 -0300

    update

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ DIR_LIB=/var/lib/sctunnel_client
 
 
 
+################################################################
+# FUNCTIONS
 print_header() {
   local title="$1"
   echo >&2
@@ -17,37 +19,34 @@ print_header() {
   echo "-------------------------------------------------" >&2
   # echo >&2
 }
-
 print_footer() {
   echo "-------------------------------------------------" >&2
 }
 
-# arp-scan
+################################################################
+# ARP-SCAN INSTALL
 print_header "ARP-SCAN INSTALL"
-
 bash "${DIR_LIB}/setup_arp_scan_sem_pedir_senha.sh"
 print_footer
 
-# Define o EXEC_CLIENTE_PATH do comando
+
+################################################################
+# EXEC_CLIENTE
 print_header "EXEC_CLIENTE INSTALL"
-
 EXEC_CLIENTE_PATH="/usr/local/bin/exec_cliente"
-
-# Cria o script do comando
 sudo tee "$EXEC_CLIENTE_PATH" > /dev/null <<'EOF'
 #!/bin/bash
 bash ${DIR_LIB}/trocar_cliente.sh "$1"
 EOF
-
-# Dá permissão de execução
 sudo chmod +x "$EXEC_CLIENTE_PATH"
-
 echo "✅ Comando 'exec_cliente' instalado com sucesso!"
 echo ""
 echo "📢 Agora você pode usar: exec_cliente <cliente_id>"
 print_footer
 
-# Verifica se --install_crons está entre os argumentos
+
+################################################################
+# CRONS
 INSTALL_CRONS=false
 REMOVE_CRONS=false
 CRONPATH="/etc/cron.d/sctunnel"
@@ -62,6 +61,8 @@ for arg in "$@"; do
 done
 
 
+################################################################
+# REMOVE_CRONS
 if $REMOVE_CRONS; then
   print_header "CRONS REMOVE"
   if [[ -f "$CRONPATH" ]]; then
@@ -74,6 +75,8 @@ if $REMOVE_CRONS; then
 fi
 
 
+################################################################
+# INSTALL_CRONS
 if $INSTALL_CRONS; then
   print_header "CRONS INSTALL"
   echo "🕒 Instalando cron jobs..."
@@ -82,10 +85,8 @@ if $INSTALL_CRONS; then
 */1 * * * * root bash ${DIR_LIB}/exec.sh >> ${DIR_LIB}/logs/cron.txt 2>&1
 */30 * * * * root /usr/bin/systemctl restart NetworkManager >> ${DIR_LIB}/logs/rede.log 2>&1
 EOF
-
   sudo chmod 644 "$CRONPATH"
   sudo chown root:root "$CRONPATH"
-
   echo "✅ Cron jobs instalados com sucesso em:"
   echo "$CRONPATH"
   print_footer
