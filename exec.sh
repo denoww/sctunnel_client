456692eae948d47623c30ee29d0fc6bbd14b5dbc
Rodrigo <denoww@gmail.com>
2025-05-12 08:11:55 -0300

    update

--- exec.sh
+++ exec.sh
@@ -1,13 +1,5 @@
-
 #!/bin/bash
-
-DIR=$(dirname "$0")
-update_firmware(){
-  echo "Atualizando firmware..."
-  git config --global --add safe.directory "$DIR" 2>/dev/null || true
-  cd "$DIR" && git pull
-}
-update_firmware
+bash update_firmware.sh
 bash exec_tunnels.sh
 
 
