cca7112f2e9b17561c1dac3cd18e878acfee7cac
Rodrigo <denoww@gmail.com>
2025-05-21 20:47:07 -0300

    update

--- windows_install.bat
+++ windows_install.bat
@@ -1,18 +1,15 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-
-
-
-:: Define o diretório do script
+:: Configura caminhos
 set "APP_DIR=%~dp0"
 set "EXEC_PATH=%APP_DIR%exec.exe"
-set "VBS_PATH=%APP_DIR%executar_oculto.vbs"
 set "TAREFA_NOME=sc_tunnel"
+set "XML_PATH=%TEMP%\tarefa_sc_tunnel.xml"
 
 :: Verifica se o executável existe
 if not exist "%EXEC_PATH%" (
-    echo [ERRO] Executável não encontrado em "%EXEC_PATH%"
+    echo [ERRO] Executável não encontrado"
     pause
     exit /b 1
 )
@@ -34,133 +31,58 @@ echo [INFO] Permissão ajustada com sucesso: %PEM_PATH%
 
 
 
+:: Escapa & do caminho para uso seguro no echo
+set "CMD_ESC_EXEC_PATH=%EXEC_PATH:&=^&%"
 
+:: Gera o XML da tarefa agendada (grupo: Administradores)
+> "%XML_PATH%" echo ^<?xml version="1.0" encoding="UTF-16"?^>
+>> "%XML_PATH%" echo ^<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task"^>
+>> "%XML_PATH%" echo   ^<RegistrationInfo^>
+>> "%XML_PATH%" echo     ^<Description^>Túnel reverso SeuCondominio^</Description^>
+>> "%XML_PATH%" echo     ^<Author^>SeuCondominio^</Author^>
+>> "%XML_PATH%" echo   ^</RegistrationInfo^>
+>> "%XML_PATH%" echo   ^<Triggers^>
+>> "%XML_PATH%" echo     ^<TimeTrigger^>
+>> "%XML_PATH%" echo       ^<Repetition^>^<Interval^>PT1M^</Interval^>^<StopAtDurationEnd^>false^</StopAtDurationEnd^>^</Repetition^>
+>> "%XML_PATH%" echo       ^<StartBoundary^>2025-01-01T00:00:00^</StartBoundary^>
+>> "%XML_PATH%" echo       ^<Enabled^>true^</Enabled^>
+>> "%XML_PATH%" echo     ^</TimeTrigger^>
+>> "%XML_PATH%" echo   ^</Triggers^>
+>> "%XML_PATH%" echo   ^<Principals^>
+>> "%XML_PATH%" echo     ^<Principal id="Author"^>
+>> "%XML_PATH%" echo       ^<GroupId^>S-1-5-32-544^</GroupId^>
+>> "%XML_PATH%" echo       ^<RunLevel^>HighestAvailable^</RunLevel^>
+>> "%XML_PATH%" echo     ^</Principal^>
+>> "%XML_PATH%" echo   ^</Principals^>
+>> "%XML_PATH%" echo   ^<Settings^>
+>> "%XML_PATH%" echo     ^<MultipleInstancesPolicy^>IgnoreNew^</MultipleInstancesPolicy^>
+>> "%XML_PATH%" echo     ^<DisallowStartIfOnBatteries^>false^</DisallowStartIfOnBatteries^>
+>> "%XML_PATH%" echo     ^<AllowStartOnDemand^>true^</AllowStartOnDemand^>
+>> "%XML_PATH%" echo     ^<Enabled^>true^</Enabled^>
+>> "%XML_PATH%" echo     ^<Hidden^>false^</Hidden^>
+>> "%XML_PATH%" echo     ^<ExecutionTimeLimit^>PT0S^</ExecutionTimeLimit^>
+>> "%XML_PATH%" echo   ^</Settings^>
+>> "%XML_PATH%" echo   ^<Actions Context="Author"^>
+>> "%XML_PATH%" echo     ^<Exec^>
+>> "%XML_PATH%" echo       ^<Command^>^&quot;!CMD_ESC_EXEC_PATH!^&quot;^</Command^>
+>> "%XML_PATH%" echo     ^</Exec^>
+>> "%XML_PATH%" echo   ^</Actions^>
+>> "%XML_PATH%" echo ^</Task^>
 
-
-rem :: Escapa caracteres XML perigosos como &
-rem set "ESC_EXEC_PATH=%EXEC_PATH:&=&amp;%"
-rem :: Cria XML da tarefa agendada
-rem > "%XML_PATH%" echo ^<?xml version="1.0" encoding="UTF-16"?^>
-rem >> "%XML_PATH%" echo ^<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task"^>
-rem >> "%XML_PATH%" echo   ^<RegistrationInfo^>
-rem >> "%XML_PATH%" echo     ^<Description^>Túnel reverso SeuCondominio^</Description^>
-rem >> "%XML_PATH%" echo     ^<Author^>SeuCondominio^</Author^>
-rem >> "%XML_PATH%" echo   ^</RegistrationInfo^>
-rem >> "%XML_PATH%" echo   ^<Triggers^>
-rem >> "%XML_PATH%" echo     ^<TimeTrigger^>
-rem >> "%XML_PATH%" echo       ^<Repetition^>^<Interval^>PT1M^</Interval^>^<StopAtDurationEnd^>false^</StopAtDurationEnd^>^</Repetition^>
-rem >> "%XML_PATH%" echo       ^<StartBoundary^>2025-01-01T00:00:00^</StartBoundary^>
-rem >> "%XML_PATH%" echo       ^<Enabled^>true^</Enabled^>
-rem >> "%XML_PATH%" echo     ^</TimeTrigger^>
-rem >> "%XML_PATH%" echo   ^</Triggers^>
-rem >> "%XML_PATH%" echo   ^<Principals^>
-rem >> "%XML_PATH%" echo     ^<Principal id="Author"^>
-rem >> "%XML_PATH%" echo       ^<RunLevel^>HighestAvailable^</RunLevel^>
-rem >> "%XML_PATH%" echo     ^</Principal^>
-rem >> "%XML_PATH%" echo   ^</Principals^>
-rem >> "%XML_PATH%" echo   ^<Settings^>
-rem >> "%XML_PATH%" echo     ^<MultipleInstancesPolicy^>IgnoreNew^</MultipleInstancesPolicy^>
-rem >> "%XML_PATH%" echo     ^<AllowStartOnDemand^>true^</AllowStartOnDemand^>
-rem >> "%XML_PATH%" echo     ^<Enabled^>true^</Enabled^>
-rem >> "%XML_PATH%" echo     ^<Hidden^>false^</Hidden^>
-rem >> "%XML_PATH%" echo     ^<ExecutionTimeLimit^>PT0S^</ExecutionTimeLimit^>
-rem >> "%XML_PATH%" echo   ^</Settings^>
-rem >> "%XML_PATH%" echo   ^<Actions Context="Author"^>
-rem >> "%XML_PATH%" echo     ^<Exec^>
-rem >> "%XML_PATH%" echo       ^<Command^>%ESC_EXEC_PATH%^</Command^>
-rem >> "%XML_PATH%" echo     ^</Exec^>
-rem >> "%XML_PATH%" echo   ^</Actions^>
-rem >> "%XML_PATH%" echo ^</Task^>
-rem :: Remove a tarefa se já existir
-rem schtasks /delete /tn "%TAREFA_NOME%" /f >nul 2>&1
-rem :: Cria a nova tarefa agendada
-rem rem schtasks /create /tn "%TAREFA_NOME%" /xml "%XML_PATH%" /ru SYSTEM /f
-rem schtasks /create /tn "%TAREFA_NOME%" /xml "%XML_PATH%" /f
-
-rem if %errorlevel% equ 0 (
-rem     echo [OK] Tarefa agendada com sucesso para: %EXEC_PATH%
-rem ) else (
-rem     echo [ERRO] Falha ao agendar tarefa. Código: %errorlevel%
-rem )
-
-
-
-
-
-:: Remove a tarefa existente, se houver
+:: Remove tarefa antiga se existir
 schtasks /delete /tn "%TAREFA_NOME%" /f >nul 2>&1
 
-
-schtasks /create ^
-  /tn "%TAREFA_NOME%" ^
-  /tr "\"%APP_DIR%exec.exe\"" ^
-  /sc minute ^
-  /mo 1 ^
-  /ru SYSTEM ^
-  /f
-
-rem schtasks /create ^
-rem   /tn "%TAREFA_NOME%" ^
-rem   /xml "%APP_DIR%windows_permissao_agendador_tarefas.xml" ^
-rem   /ru SYSTEM ^
-rem   /f
-
-:: Cria a nova tarefa agendada que executa o script VBS
-rem :: Cria o script VBScript para execução oculta
-rem echo Set WshShell = CreateObject("WScript.Shell") > "%VBS_PATH%"
-rem echo WshShell.CurrentDirectory = "%APP_DIR%" >> "%VBS_PATH%"
-rem echo WshShell.Run "exec.exe", 0, False >> "%VBS_PATH%"
-rem schtasks /create ^
-rem   /tn "%TAREFA_NOME%" ^
-rem   /tr "wscript.exe \"%VBS_PATH%\"" ^
-rem   /sc minute ^
-rem   /mo 1 ^
-rem   /ru SYSTEM ^
-rem   /f
-
-
-
-
+:: Cria a nova tarefa com base no XML
+schtasks /create /tn "%TAREFA_NOME%" /xml "%XML_PATH%" /f
 
 if %errorlevel% equ 0 (
-    echo Tarefa agendada com sucesso para: %EXEC_PATH%
+    echo.
+    echo ✅ Tarefa criada com sucesso usando grupo 'Administradores'.
+    echo 🔄 A execução ocorrerá a cada 1 minuto. Verifique o log em log.logs
 ) else (
-    echo Falha ao agendar tarefa. Código: %errorlevel%
+    echo.
+    echo ❌ [ERRO] Falha ao criar a tarefa. Código: %errorlevel%
 )
-
-
-
-
-
-
-
-rem echo  Instalando e ativando OpenSSH Server...
-
-rem :: Verifica se é Admin
-rem net session >nul 2>&1
-rem if %errorlevel% neq 0 (
-rem   echo  Este script precisa ser executado como administrador.
-rem   pause
-rem   exit /b
-rem )
-
-rem :: Instala o OpenSSH.Server via PowerShell
-rem echo  Instalando OpenSSH Server...
-rem powershell -Command "Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0"
-
-rem :: Inicia e configura o serviço SSH
-rem echo  Iniciando serviço SSH...
-rem sc start sshd
-rem sc config sshd start=auto
-
-rem :: Libera porta 22 no firewall
-rem echo  Liberando porta 22 no firewall...
-rem powershell -Command "New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22"
-
-
-
-
-
 
 
 endlocal
