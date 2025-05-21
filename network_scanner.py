98bf3d3673bcd48e009ebc14e5fd44c0994dae32
Rodrigo <denoww@gmail.com>
2025-05-21 09:08:57 -0300

    update

--- network_scanner.py
+++ network_scanner.py
@@ -21,7 +21,7 @@ def varredura_arp(interface, subnet):
         logging.info(f"{len(ans)} respostas recebidas de ARP")
         resultados = []
         for snd, rcv in ans:
-            print(f"\033[0;32mIP={rcv.psrc} MAC={rcv.hwsrc}\033[0m")
+            logging.info(f"\033[0;32mIP={rcv.psrc} MAC={rcv.hwsrc}\033[0m")
             # logging.info(f"Recebido: IP={rcv.psrc} MAC={rcv.hwsrc}")
             resultados.append({"ip": rcv.psrc, "mac": rcv.hwsrc})
         return resultados
