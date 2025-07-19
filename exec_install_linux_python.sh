951d603b59fd3a32ba055e1b98ecbdb8de2c61fd
Rodrigo <denoww@gmail.com>
2025-07-19 12:24:27 -0300

    update

--- exec_install_linux_python.sh
+++ exec_install_linux_python.sh
@@ -12,38 +12,7 @@ source "$DIR_LIB/env.sh"
 
 #################################
 # python sem sudo
-if command -v setcap >/dev/null; then
-    # PYTHON_REAL=$(readlink -f "$(command -v "$pybin")")
-    # PYTHON_REAL=$(readlink -f "$(which python3.10)")
-    echo "[INFO] Configurando permissões para $PYTHON_REAL usar raw sockets..."
-    sudo setcap cap_net_raw+eip "$PYTHON_REAL"
-
-    if getcap "$PYTHON_REAL" | grep -q cap_net_raw; then
-      echo "[OK] Permissão cap_net_raw aplicada com sucesso em $PYTHON_REAL"
-    else
-      echo "[ERRO] Falha ao aplicar cap_net_raw em $PYTHON_REAL"
-      echo "       Verifique se você está usando um interpretador do sistema e não de uma venv."
-      exit 1
-    fi
-
-  # PYTHON_REAL=$(readlink -f "$(which python3)")
-  # # PYTHON_REAL=$(readlink -f "$(command -v python3)")
-  # # PYTHON_REAL="/usr/bin/python3"
-  # echo "[INFO] Configurando permissões para Python usar raw sockets..."
-  # sudo setcap cap_net_raw+eip "$PYTHON_REAL"
-
-  # # Verifica se a permissão foi aplicada com sucesso
-  # if getcap "$PYTHON_REAL" | grep -q cap_net_raw; then
-  #   echo "[OK] Permissão cap_net_raw aplicada com sucesso em $PYTHON_REAL"
-  # else
-  #   echo "[ERRO] Falha ao aplicar cap_net_raw em $PYTHON_REAL"
-  #   echo "       Verifique se você está usando um Python do sistema e não de uma venv."
-  #   exit 1
-  # fi
-else
-  echo "[ERRO] 'setcap' não encontrado. Scapy pode não funcionar sem sudo." >&2
-  exit 1
-fi
+bash cap_net_raw.sh
 #################################
 
 # Corrige permissões e dependências do sistema de sugestões (opcional, mas limpa logs)
